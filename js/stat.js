44250ce4d81956c25fb4c84828748de63a9335ff
izasorin <izasorin@gmail.com>
2020-09-25 19:34:25 +0300

    Незначительные исправления

--- js/stat.js
+++ js/stat.js
@@ -5,15 +5,15 @@ const CLOUD_HEIGHT = 270;
 const CLOUD_X = 100;
 const CLOUD_Y = 10;
 const GAP = 10;
-const CLOUD_COLOR = '#ffffff';
-const CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+const CLOUD_COLOR = `#ffffff`;
+const CLOUD_SHADOW_COLOR = `rgba(0, 0, 0, 0.7)`;
 const BAR_WIDTH = 40;
 const BAR_MAX_HEIGHT = -150;
 const BAR_GAP = 50;
-const PLAYER_BAR_COLOR = 'rgba(255, 0, 0, 1)';
-const PLAYER_NAME = 'Вы';
-const FONT = '16px PT Mono';
-const TEXT_COLOR = '#000000';
+const PLAYER_BAR_COLOR = `rgba(255, 0, 0, 1)`;
+const PLAYER_NAME = `Вы`;
+const FONT = `16px PT Mono`;
+const TEXT_COLOR = `#000000`;
 const TEXT_HEIGHT = 10;
 
 const renderCloud = (ctx, x, y, color) => {
@@ -24,7 +24,7 @@ const renderCloud = (ctx, x, y, color) => {
 const renderMessage = (ctx, x, y, text) => {
   ctx.fillStyle = TEXT_COLOR;
   ctx.font = FONT;
-  ctx.textBaseline = 'hanging';
+  ctx.textBaseline = `hanging`;
   ctx.fillText(text, x, y);
 };
 
@@ -45,8 +45,8 @@ const getRandomNumber = () => Math.floor(Math.random() * 90);
 window.renderStatistics = (ctx, players, times) => {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_SHADOW_COLOR);
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
-  renderMessage(ctx, CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2, 'Ура вы победили!');
-  renderMessage(ctx, CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2 + GAP * 2, 'Список результатов:');
+  renderMessage(ctx, CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2, `Ура вы победили!`);
+  renderMessage(ctx, CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2 + GAP * 2, `Список результатов:`);
 
   const maxTime = getMaxElement(times);
 
@@ -55,14 +55,14 @@ window.renderStatistics = (ctx, players, times) => {
 
     ctx.fillStyle = TEXT_COLOR;
     ctx.font = FONT;
-    ctx.textBaseline = 'hanging';
+    ctx.textBaseline = `hanging`;
     ctx.fillText(players[i], CLOUD_X + BAR_GAP - GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - GAP * 2);
     ctx.fillText(Math.ceil(times[i]), CLOUD_X + BAR_GAP - GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - BAR_GAP - Math.abs(barHeight));
 
     if (players[i] === PLAYER_NAME) {
       ctx.fillStyle = PLAYER_BAR_COLOR;
     } else {
-      ctx.fillStyle = 'hsl(250, ' + getRandomNumber() + '%, 50%)';
+      ctx.fillStyle = `hsl(250, ` + getRandomNumber() + `%, 50%)`;
     }
     ctx.fillRect(CLOUD_X + BAR_GAP - GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - GAP * 2 - TEXT_HEIGHT, BAR_WIDTH, barHeight);
   }
